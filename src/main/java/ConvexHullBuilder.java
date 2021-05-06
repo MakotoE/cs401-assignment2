aa87fcbf50902e1fa031e569d9be2ed5d1bf52c5
Makoto <makoto@emurasoft.com>
2021-05-06 11:40:46 -0700

    Tests

--- src/main/java/ConvexHullBuilder.java
+++ src/main/java/ConvexHullBuilder.java
@@ -1,31 +1,36 @@
 import edu.princeton.cs.algs4.Point2D;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Stack;
 
-public class ConvexHullBuilder implements Iterator<Point2D> {
-	static Stack<Point2D> get_stack(ArrayList<Point2D> pointList) {
-		// TODO empty list case
-		var lowest = pointList.get(0);
-		for (var point : pointList.subList(1, pointList.size())) {
+public class ConvexHullBuilder {
+	static Stack<Point2D> get_stack(ArrayList<Point2D> points) {
+		if (points.size() == 0) {
+			return new Stack<>();
+		}
+
+		// Find lowest point
+		var lowest = points.get(0);
+		for (var point : points.subList(1, points.size())) {
 			if (point.y() < lowest.y() || (point.y() == lowest.y() && point.x() < lowest.x())) {
 				lowest = point;
 			}
 		}
 
-		pointList.sort(lowest.polarOrder());
+		// Sort by polar angle with lowest point
+		points.sort(lowest.polarOrder());
 
-		for (int i = 0; i < pointList.size() - 1; i++) {
+		// Remove points with same polar angle
+		for (int i = 0; i < points.size() - 1; i++) {
 			if (
-				lowest != pointList.get(i)
-				&& lowest != pointList.get(i + 1)
-				&& Point2D.ccw(lowest, pointList.get(i), pointList.get(i + 1)) == 0
+				lowest != points.get(i)
+				&& lowest != points.get(i + 1)
+				&& Point2D.ccw(lowest, points.get(i), points.get(i + 1)) == 0
 			) {
-				if (lowest.distanceTo(pointList.get(i)) > lowest.distanceTo(pointList.get(i + 1))) {
-					pointList.remove(i + 1);
+				if (lowest.distanceTo(points.get(i)) > lowest.distanceTo(points.get(i + 1))) {
+					points.remove(i + 1);
 				} else {
-					pointList.remove(i);
+					points.remove(i);
 				}
 				i--;
 			}
@@ -33,10 +38,11 @@ public class ConvexHullBuilder implements Iterator<Point2D> {
 
 		var stack = new Stack<Point2D>();
 
-		for (var point : pointList) {
+		// Find path with only left turns
+		for (var point : points) {
 			while (
 				stack.size() > 1
-					&& Point2D.ccw(stack.get(stack.size() - 2), stack.peek(), point) <= 0
+				&& Point2D.ccw(stack.get(stack.size() - 2), stack.peek(), point) <= 0
 			) {
 				stack.pop();
 			}
@@ -45,20 +51,13 @@ public class ConvexHullBuilder implements Iterator<Point2D> {
 		return stack;
 	}
 
-	public ConvexHullBuilder(ArrayList<Point2D> pointList) {
-
+	public ConvexHullBuilder(ArrayList<Point2D> points) {
+		this.stack = get_stack(points);
 	}
 
 	public Iterable<Point2D> hull() {
-		//noinspection unchecked
-		return (Iterable<Point2D>) this;
+		return this.stack;
 	}
 
-	public boolean hasNext() {
-		return false;
-	}
-
-	public Point2D next() {
-		return null;
-	}
+	private final Stack<Point2D> stack;
 }
--- src/main/java/ConvexHullBuilderTest.java
+++ src/main/java/ConvexHullBuilderTest.java
@@ -1,21 +1,85 @@
 import edu.princeton.cs.algs4.Point2D;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.Assert.assertArrayEquals;
+
+@RunWith(Enclosed.class)
 public class ConvexHullBuilderTest {
-	@Test
-	public void test() {
-		var points = new Point2D[]{
-			new Point2D(-2, 1),
-			new Point2D(-1, -1),
-			new Point2D(1, 1),
-			new Point2D(2, 0),
-			new Point2D(0, 0),
-		};
-		for (var point : ConvexHullBuilder.get_stack(new ArrayList<>(Arrays.asList(points)))) {
-			System.out.println(point);
+
+	@RunWith(Parameterized.class)
+	public static class get_stack {
+		@Parameterized.Parameters
+		public static Collection<Object> data() {
+			return Arrays.asList(new Object[][]{
+				{
+					new Point2D[]{},
+					new Point2D[]{},
+				},
+				{
+					new Point2D[]{
+						new Point2D(0, 0),
+					},
+					new Point2D[]{
+						new Point2D(0, 0),
+					},
+				},
+				{
+					new Point2D[]{
+						new Point2D(0, 0),
+						new Point2D(1, 0),
+					},
+					new Point2D[]{
+						new Point2D(0, 0),
+						new Point2D(1, 0),
+					},
+				},
+				{
+					new Point2D[]{
+						new Point2D(0, 0),
+						new Point2D(1, 1),
+						new Point2D(1, 0),
+					},
+					new Point2D[]{
+						new Point2D(0, 0),
+						new Point2D(1, 0),
+						new Point2D(1, 1),
+					},
+				},
+				{
+					new Point2D[]{
+						new Point2D(-2, 1),
+						new Point2D(-1, -1),
+						new Point2D(1, 1),
+						new Point2D(2, 0),
+						new Point2D(0, 0),
+					},
+					new Point2D[]{
+						new Point2D(-2, 1),
+						new Point2D(-1, -1),
+						new Point2D(2, 0),
+						new Point2D(1, 1),
+					},
+				}
+			});
+		}
+
+		@Parameterized.Parameter
+		public Point2D[] points;
+
+		@Parameterized.Parameter(1)
+		public Point2D[] expected;
+
+		@Test
+		public void test() {
+			var result = ConvexHullBuilder.get_stack(new ArrayList<>(Arrays.asList(points)));
+			assertArrayEquals(result.toArray(), expected);
 		}
 	}
 }
